63d541f962891a40647898acf96e206558411cfb
VitorB2002 <vitorb.santos2002@gmail.com>
2024-08-13 14:49:38 -0300

    feat: adicionado tabela para usuários

--- MyGov/services/models.py
+++ MyGov/services/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    """Model for users"""
+    name = models.CharField(max_length=100, default='')
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=24, default='')
+
+    def __str__(self):
+        """String representation of the user"""
+        return self.name
