fdb04a49dd6032f0485cdaf428f722abea664096
VitorB2002 <vitorb.santos2002@gmail.com>
2024-08-15 18:43:00 -0300

    feat: adicionado fluxo de solicitar retrovenda via codigo do imovel

--- MyGov/services/views.py
+++ MyGov/services/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import DeclaracaoRetroVenda
 
 def index(request):
     context = {
         'message': "Hello, world. You're at the services index."
     }
     return render(request, 'index.html', context)
+
+def retrovenda(request):
+    if request.method == 'POST':
+        codigo_imovel = request.POST.get('codigo_imovel')
+        declaracao = DeclaracaoRetroVenda.objects.filter(codigo_imovel=codigo_imovel)
+        if declaracao:
+            return render(request, 'info.html', {'codigo_imovel': codigo_imovel, 'declaracao': declaracao[0], 'endereco': declaracao[0].endereco})
+        return render(request, 'retrovenda.html', {'codigo_imovel': codigo_imovel})
+    return render(request, 'retrovenda.html')
