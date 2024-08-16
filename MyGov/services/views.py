95c782b66de33083d5eebb351d055fc7c3323e5e
VitorB2002 <vitorb.santos2002@gmail.com>
2024-08-16 14:13:25 -0300

    feat: adicionado possibilidade de buscar retrovenda a partir do endereço

--- MyGov/services/views.py
+++ MyGov/services/views.py
@@ -10,8 +10,17 @@ def index(request):
 def retrovenda(request):
     if request.method == 'POST':
         codigo_imovel = request.POST.get('codigo_imovel')
-        declaracao = DeclaracaoRetroVenda.objects.filter(codigo_imovel=codigo_imovel)
-        if declaracao:
-            return render(request, 'info.html', {'codigo_imovel': codigo_imovel, 'declaracao': declaracao[0], 'endereco': declaracao[0].endereco})
-        return render(request, 'retrovenda.html', {'codigo_imovel': codigo_imovel})
+        if codigo_imovel == None:
+            endereco = request.POST.get('endereco')
+            declaracao = DeclaracaoRetroVenda.objects.filter(endereco=endereco)
+            if declaracao:
+                return render(request, 'info.html', {'declaracao': declaracao[0], 'endereco': endereco})
+
+            return render(request, 'info.html', {'declaracao': None})
+        else:
+            declaracao = DeclaracaoRetroVenda.objects.filter(codigo_imovel=codigo_imovel)
+            if declaracao:
+                return render(request, 'info.html', {'codigo_imovel': codigo_imovel, 'declaracao': declaracao[0], 'endereco': declaracao[0].endereco})
+
+            return render(request, 'info.html', {'declaracao': None})
     return render(request, 'retrovenda.html')
